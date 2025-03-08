949c99652588824c760ec3e7f1c5a4fee2fde60f
Mayur Athavale <81901817+mayurathavale18@users.noreply.github.com>
2025-03-08 15:08:37 +0530

    changes

--- Algorithm/pdt_link_scraper.py
+++ Algorithm/pdt_link_scraper.py
@@ -1,9 +1,3 @@
-# Furniture.ca Living Room Products Scrapper
-
-# To use phantomjs via selenium, you need:
-# 1) Install NodeJS: brew install node or https://nodejs.org/en/
-# 2) Using Node's package manager install NodeJS: npm -g install phantomjs-prebuilt (admin access)
-
 import os
 import logging
 import json
@@ -13,81 +7,83 @@ from random import randint
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+
 logging.basicConfig(format='%(asctime)s - %(filename)s - %(message)s', level=logging.INFO)
 
-ROOT = os.environ.get("ROOT")
-# main_folder = Path(ROOT, 'Algorithm')
-domain = 'www.furniture.ca'
-item_per_page = 24  # specifically for this web
-categories = ['living-room-packages', 'sofas', 'loveseats', 'sectionals', 'chairs-chaises', 'recliners',
-              'ottomans-benches', 'cabinets-shelving', 'tv-stands-tv-mounts', 'coffee-tables',
-              'sofa-console-tables', 'end-accent-tables']  # specifically for this web
+# Set up root directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+default_root = os.path.dirname(script_dir)
+ROOT = os.environ.get("ROOT", default_root)
 
+domain = "www.furniture.ca"
+item_per_page = 24
+categories = ["living-room-packages", "sofas", "loveseats", "sectionals", "chairs-chaises", "recliners",
+              "ottomans-benches", "cabinets-shelving", "tv-stands-tv-mounts", "coffee-tables",
+              "sofa-console-tables", "end-accent-tables"]
 
 def main():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")  # Required for some Windows setups
+    # chrome_options.add_argument("--headless")  # Disable for debugging
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    
-    # Initialize the Chrome driver with options
+
     driver = webdriver.Chrome(options=chrome_options)
-    for k, j in enumerate(categories):
-        cat = j
+
+    for k, cat in enumerate(categories):
         cat_links = []
-        logging.info('Start scraping category {}'.format(cat))
-        # NOTE : There's chance that some elements are non available each time, due to unsure reason
-        # (visibility setting / waiting certain element to load page). Besides, some category will have less data
-        # SOLUTION: Change while i <= iters to run each request for multiple times
-        i = 0  # set # of trials
-        offset = item_per_page  # based on certain website format
-        while i <= 10:
-            logging.info('Start iteration {}'.format(i))
-            logging.info('Start offset {}'.format(offset))
-            # TODO: selenium depreation warning
-            link = 'https://www.furniture.ca/collections/furniture-living-room-' + cat + '?offset=' + str(offset)
-            # driver = webdriver.PhantomJS(
-            #     executable_path='/usr/local/lib/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs')
-            rdm = randint(10, 36)
+        logging.info(f"Start scraping category {cat}")
+        offset = 0  # Start from 0 instead of item_per_page
+
+        for i in range(10):  # Retry scraping up to 10 times
+            logging.info(f"Start iteration {i}, Offset {offset}")
+            link = f"https://www.furniture.ca/collections/furniture-living-room-{cat}?offset={offset}"
             driver.get(link)
-            page = BeautifulSoup(driver.page_source, 'html.parser')
-            r = re.compile(r'myshopify.com/products')  # specifically for this web
-            for a in page.findAll('a', href=r):
-                # find link
-                link = a['href']
-                # check if link is new
-                if link not in cat_links:
-                    logging.info('Found new link {}'.format(link))
-                    cat_links.append(link)
-                else:
-                    logging.warning('Found link already exists so skipped')
 
-            # check if good to go to next page
-            if (len(cat_links) % item_per_page == 0) and (len(cat_links) != 0):
-                offset += item_per_page
-            i += 1
-            logging.info('Collected {} items'.format(len(cat_links)))
-            sleep(rdm)
+            try:
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.TAG_NAME, "body"))
+                )
+            except:
+                logging.warning("Page did not load properly")
+                continue  # Skip this iteration if page didn't load
 
-        # write json file
-        product_link = {'category': cat, 'link': cat_links}
-        with open(Path(ROOT, 'links_cat_{}.json'.format(k)), 'w') as outfile:
-            json.dump(product_link, outfile)
-        logging.info('Finish scraping category {}'.format(cat))
+            page = BeautifulSoup(driver.page_source, "html.parser")
+            r = re.compile(r"myshopify.com/products")
 
-    # combine all category links
-    logging.info('Start combining all category links')
-    product_links = []
-    for i in range(0, len(categories)):
-        with open(Path(ROOT, 'links_cat_{}.json'.format(i))) as outfile:
-            f = json.load(outfile)
-            product_links.append(f)
-    with open(Path(ROOT, 'product_links.json'), 'w') as outfile:
-        json.dump(product_links, outfile)
-    logging.info('Finish combining all category links')
+            for a in page.findAll("a", href=r):
+                product_link = a["href"]
+                if product_link not in cat_links:
+                    logging.info(f"Found new link {product_link}")
+                    cat_links.append(product_link)
 
+            if len(cat_links) % item_per_page == 0 and len(cat_links) > 0:
+                offset += item_per_page  # Move to next page
+
+            logging.info(f"Collected {len(cat_links)} items")
+            sleep(randint(2, 6))  # Reduce sleep time
+
+        with open(Path(ROOT, f"links_cat_{k}.json"), "w") as outfile:
+            json.dump({"category": cat, "links": cat_links}, outfile)
+
+        logging.info(f"Finish scraping category {cat}")
+
+    logging.info("Start combining all category links")
+    all_product_links = []
+
+    for i in range(len(categories)):
+        with open(Path(ROOT, f"links_cat_{i}.json")) as infile:
+            all_product_links.append(json.load(infile))
+
+    with open(Path(ROOT, "product_links.json"), "w") as outfile:
+        json.dump(all_product_links, outfile)
+
+    logging.info("Finish combining all category links")
+    driver.quit()
 
 if __name__ == "__main__":
     main()
